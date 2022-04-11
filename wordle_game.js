e949713834537168e650aa9c74f720b8fe1ea3a5
ObviouslyLuuk <54840183+ObviouslyLuuk@users.noreply.github.com>
2022-04-11 13:23:53 +0200

    made separate .js file for constants; now loading separate files for allowed words and mystery words; also loading separate files for different word length; also having the word probs precomputed so nothing needs to be done on the spot;

--- wordle_game.js
+++ wordle_game.js
@@ -1,4 +1,4 @@
-
+// CONSTANTS
 const emoji_dict_code = {
     "correct": "\uD83D\uDFE9",
     "present": "\uD83D\uDFE8",
@@ -10,7 +10,7 @@ const emoji_dict = {
     "absent": "⬛",
 }
 
-var TWITTER_SHARE_LINK = `https://twitter.com/intent/tweet?text=`
+const TWITTER_SHARE_LINK = `https://twitter.com/intent/tweet?text=`
 
 const HTML_DICT = {
     " ": "%20",
@@ -20,6 +20,35 @@ const HTML_DICT = {
 const latin_alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 const greek_alphabet = ['\u03B1', '\u03B2', '\u03B3', '\u03B4', '\u03B5', '\u03B6', '\u03B7', '\u03B8', '\u03D1', '\u03B9', '\u03BA', '\u03BB', '\u03BC', '\u03BD', '\u03BE', '\u03BF', '\u03C0', '\u03D6', '\u03C1', '\u03C2', '\u03C3', '\u03C4', '\u03C5', '\u03C6', '\u03C7', '\u03C8', '\u03C9']
 const greek_chars_dict = {"\u03b1": "a", "\u03b2": "b", "\u03b3": "g", "\u03b4": "d", "\u03b5": "e", "\u03b6": "z", "\u03b7": "h", "\u03b8": "u", "\u03b9": "i", "\u03ba": "k", "\u03bb": "l", "\u03bc": "m", "\u03bd": "n", "\u03be": "j", "\u03bf": "o", "\u03c0": "p", "\u03c1": "r", "\u03c3": "s", "\u03c2": "q", "\u03c4": "t", "\u03c5": "y", "\u03c6": "f", "\u03c7": "x", "\u03c8": "c", "\u03c9": "v", "a": "\u03b1", "b": "\u03b2", "g": "\u03b3", "d": "\u03b4", "e": "\u03b5", "z": "\u03b6", "h": "\u03b7", "u": "\u03b8", "i": "\u03b9", "k": "\u03ba", "l": "\u03bb", "m": "\u03bc", "n": "\u03bd", "j": "\u03be", "o": "\u03bf", "p": "\u03c0", "r": "\u03c1", "s": "\u03c3", "q": "\u03c2", "t": "\u03c4", "y": "\u03c5", "f": "\u03c6", "x": "\u03c7", "c": "\u03c8", "v": "\u03c9"}
+
+
+const QUESTION_MARK = `
+<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16">
+    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+    <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>
+</svg>`
+const BACKSPACE_0 = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-backspace-fill" viewBox="0 0 16 16">
+    <path d="M15.683 3a2 2 0 0 0-2-2h-7.08a2 2 0 0 0-1.519.698L.241 7.35a1 1 0 0 0 0 1.302l4.843 5.65A2 2 0 0 0 6.603 15h7.08a2 2 0 0 0 2-2V3zM5.829 5.854a.5.5 0 1 1 .707-.708l2.147 2.147 2.146-2.147a.5.5 0 1 1 .707.708L9.39 8l2.146 2.146a.5.5 0 0 1-.707.708L8.683 8.707l-2.147 2.147a.5.5 0 0 1-.707-.708L7.976 8 5.829 5.854z"/>
+</svg>`
+const BACKSPACE_1 = `
+<svg xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 0 24 24" width="24">
+    <path fill="currentColor" d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"></path>
+</svg>`
+const TWITTER_ICON = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+  <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+</svg>`
+const COPY_ICON = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+  <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+  <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+</svg>`
+const BARCHART_ICON = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bar-chart-line-fill" viewBox="0 0 16 16">
+  <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1V2z"/>
+</svg>`
+///////////////////////////////
 
 var alphabet = latin_alphabet
 
@@ -73,9 +102,9 @@ var accent_dict = {
     '\u03ce': '\u03c9',
     '\u03c2': '\u03c3',
 }
-for (let [k, v] of Object.entries(accent_dict)) {
-    accent_dict[k.toUpperCase()] = v.toUpperCase()
-}
+// for (let [k, v] of Object.entries(accent_dict)) {
+//     accent_dict[k.toUpperCase()] = v.toUpperCase()
+// }
 
 var keydict = {
     13: "enter",
@@ -90,34 +119,8 @@ for (let [keycode, char] of Object.entries(keydict)) {
 keydict_greek[13] = "enter"
 keydict_greek[8] = "backspace"
 
-const QUESTION_MARK = `
-<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16">
-    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-    <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>
-</svg>`
-const BACKSPACE_0 = `
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-backspace-fill" viewBox="0 0 16 16">
-    <path d="M15.683 3a2 2 0 0 0-2-2h-7.08a2 2 0 0 0-1.519.698L.241 7.35a1 1 0 0 0 0 1.302l4.843 5.65A2 2 0 0 0 6.603 15h7.08a2 2 0 0 0 2-2V3zM5.829 5.854a.5.5 0 1 1 .707-.708l2.147 2.147 2.146-2.147a.5.5 0 1 1 .707.708L9.39 8l2.146 2.146a.5.5 0 0 1-.707.708L8.683 8.707l-2.147 2.147a.5.5 0 0 1-.707-.708L7.976 8 5.829 5.854z"/>
-</svg>`
-const BACKSPACE_1 = `
-<svg xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 0 24 24" width="24">
-    <path fill="currentColor" d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"></path>
-</svg>`
-const TWITTER_ICON = `
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
-  <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
-</svg>`
-const COPY_ICON = `
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
-  <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
-  <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
-</svg>`
-const BARCHART_ICON = `
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bar-chart-line-fill" viewBox="0 0 16 16">
-  <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1V2z"/>
-</svg>`
-
-var WORDS_BY_LANG = {}
+var WORD_PROBS = {}
+var ALLOWED_WORDS = {}
 
 // Maths
 function random(seed) {
@@ -195,17 +198,26 @@ function set_visibility(element_id, vis) {
         // fade(element)
 }
 
-function load_word_probs(language) {
+function load_word_probs(language, length) {
     set_loader()
 
-    let id = `${language}_words`
-    let script = document.getElementById(id)
-    if (!script) {
-        script = create_and_append('script', document.body, id)
-        script.src = `words/word_probs_${language}.js`
-        script.type = "text/javascript"
+    let probs_id = `${language}_word_probs_L${length}`
+    let probs_script = document.getElementById(probs_id)
+    if (!probs_script) {
+        probs_script = create_and_append('script', document.body, probs_id)
+        probs_script.src = `word_sampling_distrs/${language}/${language}_distr_len=${length}.js`
+        probs_script.type = "text/javascript"
+        probs_script.charset = "UTF-8"
     }
-    return id
+    let allow_id = `${language}_allowed_L${length}`
+    let allow_script = document.getElementById(allow_id)
+    if (!allow_script) {
+        allow_script = create_and_append('script', document.body, allow_id)
+        allow_script.src = `words_allowed/${language}/${language}_allowed_len=${length}.js`
+        allow_script.type = "text/javascript"
+        allow_script.charset = "UTF-8"
+    }
+    return { probs_id, allow_id }
 }
 
 function move_element(element, new_parent) {
@@ -420,10 +432,12 @@ function apply_sigmoid(word_probs) {
 function remove_accents(word) {
     new_word = ''
     for (let i in word) {
-        if (Object.keys(accent_dict).includes(word[i])) {
-            new_word += accent_dict[word[i]]
+        c = word[i]
+        // c = c.toLowerCase()
+        if (Object.keys(accent_dict).includes(c)) {
+            new_word += accent_dict[c]
         } else {
-            new_word += word[i]
+            new_word += c
         }
     }
     return new_word
@@ -511,13 +525,12 @@ class Game {
         if (!this.hard_mode)
             return this.allowed_guesses
 
-        let word_probs = this.allowed_guesses
-        let new_word_probs = {}
-        for (let [word, prob] of Object.entries(word_probs)) {
+        let allowed_guesses = []
+        for (let word of this.allowed_guesses) {
             if (this.word_allowed(guessed_word, result, word))
-                new_word_probs[word] = prob
+                allowed_guesses.push(word)
         }
-        this.allowed_guesses = normalize_word_probs(new_word_probs)
+        this.allowed_guesses = allowed_guesses
         return this.allowed_guesses
     }
 
@@ -571,7 +584,7 @@ class Game {
 
         if (check_vocab) {
             let allowed = false
-            for (let allowed_guess of Object.keys(this.allowed_guesses)) {
+            for (let allowed_guess of this.allowed_guesses) {
                 if (guessed_word == remove_accents(allowed_guess)) {
                     allowed = true }
             }
@@ -623,23 +636,6 @@ class Game {
         return normalize_word_probs(filtered_word_probs)
     }
 
-    get_mystery_words(language, word_len) {
-        if (language == "wordle" && word_len == 5) {
-            this.mystery_words = {}
-            for (let word of WORDLE_WORDS) {
-                this.mystery_words[word] = 1 }
-            this.mystery_words = normalize_word_probs(this.mystery_words)
-        } else {
-            this.mystery_words = {}
-            // Only add lowercase words without accents (unless greek)
-            for (let [word, count] of Object.entries(this.allowed_guesses)) {
-                if (word[0] == word[0].toLowerCase() && (word == remove_accents(word) || language == "greek") ) {
-                    this.mystery_words[word] = count }
-            }
-        }
-        return this.mystery_words
-    }
-
     init_stats(attempts) {
         // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Client-side_storage
         try {
@@ -662,6 +658,19 @@ class Game {
         return this.seed == get_date_string()
     }
 
+    wait_for_reset(word_len, language) {
+        load_word_probs(language, word_len);
+        let game = this
+        let key = `${language},${word_len}`
+        let checkExist = setInterval(function() {
+            if (WORD_PROBS[key] && ALLOWED_WORDS[key]) {
+                console.log("Words loaded!")
+                clearInterval(checkExist);
+                game.reset(word_len, language); 
+            }
+        }, 10); // check every 10ms
+    }
+
     reset(word_len=null, language=null, attempts=null, hard_mode=null, seed=null) {
         set_loader()
         if (!word_len)
@@ -676,8 +685,9 @@ class Game {
         if (this.word_len != word_len || this.language != language) {
             this.word_len = word_len
             this.language = language
-            this.word_probs = WORDS_BY_LANG[this.language]
-            this.filtered_word_probs = this.filter_by_len(word_len, this.word_probs)
+            let key = `${this.language},${this.word_len}`
+            this.mystery_words = WORD_PROBS[key]
+            this.allowed_guesses = ALLOWED_WORDS[key].concat(Object.keys(this.mystery_words))
         } else {
             this.seed = parseInt(Math.random()*99999) }
 
@@ -687,13 +697,9 @@ class Game {
             this.hard_mode = hard_mode }
         if (seed != null) {
             this.seed = seed }
-        
-        this.allowed_guesses = this.filtered_word_probs
-        this.mystery_words = normalize_word_probs(apply_sigmoid(
-            this.get_mystery_words(this.language, word_len)
-        ))
-        // console.log("mystery words: ", Object.entries(this.mystery_words))
-        // console.log("allowed guesses: ", Object.keys(this.allowed_guesses))
+
+        console.log("mystery words: ", Object.entries(this.mystery_words))
+        console.log("allowed guesses: ", this.allowed_guesses)
         this.mystery_word = multinomial_sample(
             Object.keys(this.mystery_words), 
             Object.values(this.mystery_words),
@@ -701,7 +707,7 @@ class Game {
         )          
 
         this.ui.reset(word_len, this.attempts, this.language)
-        this.ui.display_message(`${this.language.toTitleCase()}<br>total allowed words: ${Object.keys(this.allowed_guesses).length}<br>total mystery words: ${Object.keys(this.mystery_words).length}`, 5000)
+        this.ui.display_message(`${this.language.toTitleCase()}<br>total allowed words: ${this.allowed_guesses.length}<br>total mystery words: ${Object.keys(this.mystery_words).length}`, 5000)
 
         document.getElementById("word_len_input").value = this.word_len
         document.getElementById(this.language+"_option").selected = true
@@ -821,16 +827,9 @@ class UI {
             option.value = lang
             option.selected = true
         }
-        select.setAttribute("onchange", `let language = this.value;
-            load_word_probs(language);
-            let checkExist = setInterval(function() {
-                if (WORDS_BY_LANG[language]) {
-                    console.log("Words loaded!");
-                    clearInterval(checkExist);
-                    document.value.reset(null, language, null);
-                    set_visibility("settings_overlay", false);
-                }
-            }, 10); // check every 10ms`)
+        select.setAttribute("onchange", `let language = this.value; let word_len = document.value.word_len;
+            document.value.wait_for_reset(word_len, language);
+            set_visibility("settings_overlay", false);`)
         subscript = create_and_append("div", parent, null, "subscript")
         subscript.innerHTML = 'Switch to another language'            
 
@@ -842,18 +841,10 @@ class UI {
             if (game.word_len == word_len) {
                 return
             }
-            if (game.language == "wordle" && word_len != 5) {
-                let language = "english"
-                load_word_probs(language)
+            let language = game.language
+            if (language == "wordle" && word_len != 5) { language = "english" }
 
-                let checkExist = setInterval(function() {
-                    if (WORDS_BY_LANG[language]) {
-                        clearInterval(checkExist);
-                        game.reset(word_len, language, null); 
-                    }
-                }, 10); // check every 10ms                
-            } else {
-            game.reset(word_len, null, null) }
+            game.wait_for_reset(word_len, language)
         })
         subscript = create_and_append("div", parent, null, "subscript")
         subscript.innerHTML = 'Change word length'        
@@ -1261,7 +1252,7 @@ class UI {
         } else if (lang == "polish") {
             rows[middle_row] += ",&#322"
         }
-        let keyboard = create_and_append('div', parent, id="keyboard")
+        let keyboard = create_and_append('div', parent, "keyboard")
         
         for (let i in rows) {
             let keys = rows[i]
@@ -1404,9 +1395,10 @@ if (urlParams.has("seed")) {
     seed = urlParams.get("seed")
 }
 
-let id = load_word_probs(language)
+let id = load_word_probs(language, word_len)
+let key = `${language},${word_len}`
 let checkExist = setInterval(function() {
-    if (WORDS_BY_LANG[language]) {
+    if (WORD_PROBS[key] && ALLOWED_WORDS[key]) {
         console.log("Words loaded!");
         clearInterval(checkExist);
         new Game(word_len, attempts, language, false, seed)
